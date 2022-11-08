c8d537e94d91fe2accddfaafd9748bf86e44149d
RealPerpy18 <arpeto@inspalamos.ct>
2022-11-08 16:14:08 +0100

    SI

--- src/Interficies/DAO.java
+++ src/Interficies/DAO.java
@@ -1,14 +1,22 @@
 package Interficies;
 
+import Objects.Bitllet;
 import Objects.Client;
 import java.util.List;
 
 public interface DAO {
-    List<Client> Tots();
+    List<Client> TotsCli();
     Client cercaClient(int id);
 
     boolean createClient(Client cli);
     boolean updateClient(Client cli);
     boolean deleteClient(Client cli);
+
+    List<Bitllet> TotsBit();
+    Client cercaBitllet(int id);
+
+    boolean createBitllet(Bitllet bit);
+    boolean updateBitllet(Bitllet bit);
+    boolean deleteBitllet(Bitllet bit);
 
 }
